74d4f822ec63ec17746d1030643d7c10f2c4cc9e
Youssef Boumlaki <yboumlak@student.1337.ma>
2024-03-16 16:04:00 +0000

    handle multiple arguments

--- src/stack_init.c
+++ src/stack_init.c
@@ -6,7 +6,7 @@
 /*   By: yboumlak <yboumlak@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 15:06:30 by yboumlak          #+#    #+#             */
-/*   Updated: 2024/03/10 16:27:38 by yboumlak         ###   ########.fr       */
+/*   Updated: 2024/03/16 15:46:15 by yboumlak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,28 +27,29 @@ void	push_stack(t_stack **stack, int num)
 
 void	stack_init(t_stack **stack, char **argv, int argc)
 {
-	int	i;
-	int	num;
+	int		i;
+	int		j;
+	char	**split_args;
+	int		num;
 
-	if (argc == 2)
+	i = 1;
+	while (i < argc)
 	{
-		i = 0;
-		argv = ft_split(argv[1], ' ');
-	}
-	else
-		i = 1;
-	while (argv[i])
-	{
-		num = ft_atoi(argv[i]);
-		if (!is_valid_integer(argv[i]))
-			free_errors(stack, "Error: Invalid number!!");
-		if (num > INT_MAX || num < INT_MIN)
-			free_errors(stack, "Error: Number out of range!!");
-		if (check_duplicate(*stack, num))
-			free_errors(stack, "Error: Duplicate found!!");
-		push_stack(stack, num);
+		split_args = ft_split(argv[i], ' ');
+		j = 0;
+		while (split_args[j])
+		{
+			num = ft_atoi(split_args[j]);
+			if (!is_valid_integer(split_args[j]))
+				free_errors(stack, "Error: Invalid number!!");
+			if (num > INT_MAX || num < INT_MIN)
+				free_errors(stack, "Error: Number out of range!!");
+			if (check_duplicate(*stack, num))
+				free_errors(stack, "Error: Duplicate found!!");
+			push_stack(stack, num);
+			j++;
+		}
+		ft_free(split_args);
 		i++;
 	}
-	if (argc == 2)
-		ft_free(argv);
 }
